2b5f750cf83a47686d4e97cf164189dabd0ae496
RamziMubarak <ramzimubarak@mhs.ac.id>
2023-04-09 22:56:21 +0700

    update

--- contact.php
+++ contact.php
@@ -7,11 +7,7 @@
     <title>Contact | Parkeer</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="css/our.css">
-    <style>
-        .col {
-            margin-bottom: 50px;
-        }
-    </style>
+    
 </head>
 
 <body class="our-body-index-and-contact">
@@ -36,7 +32,7 @@
         </nav>
         <div class="container text-center">
             <div class="row" style="margin-top: 30px;">
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -46,7 +42,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -56,7 +52,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card" style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -69,7 +65,7 @@
 
             </div>
             <div class="row">
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -79,7 +75,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/jo.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -89,7 +85,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adit.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
--- index.php
+++ index.php
@@ -8,11 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link rel="stylesheet" href="css/our.css">
-    <style>
-        .col {
-            margin-bottom: 50px;
-        }
-    </style>
+   
 </head>
 
 <body class="our-body-index-and-contact">
@@ -53,7 +49,7 @@
 
         <div class="container text-center">
             <div class="row" style="margin-top: 30px;">
-            <div class="col d-flex justify-content-center">
+            <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -63,7 +59,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -73,7 +69,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center" >
+                <div class="col our-col d-flex justify-content-center" >
                     <div class="card" style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -86,7 +82,7 @@
 
             </div>
             <div class="row">
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adin.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -96,7 +92,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/jo.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
@@ -106,7 +102,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col d-flex justify-content-center">
+                <div class="col our-col d-flex justify-content-center">
                     <div class="card " style="width: 28rem;">
                         <img src="img/adit.jpg" class="card-img-top object-fit-cover " style="height: 350px;">
                         <div class="card-body">
